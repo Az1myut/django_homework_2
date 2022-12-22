fe56cc7ee5f763b6844db58aa96a97d481686c6f
Пользователь <honeycomb0404@gmail.com>
2022-12-22 20:37:30 +0500

    Second commit

--- books_catalogue/books_manager/views.py
+++ books_catalogue/books_manager/views.py
@@ -3,10 +3,16 @@ from .models import Book
 
 # Create your views here.
 def index(request)->render:
-    books=Book.objects.all()
+    books={
+    'Harry Potter':'J W Rowling',
+    'War and Peace':'Leo Tolstoi',
+    'Swim in a Pond in the Rain':'George Saunders',
+    ' Living with a Dead Language':'Pat Pattison'
+}
+    sorted_books=dict(sorted(books.items()))
     template_='index.html'
     context={
-        'books':books,
+        'books':sorted_books,
     }
     return render(request,template_,context=context)
 
